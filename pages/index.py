63414037a1136102a5c54f115f72e2d087a82721
Brett <doffing81@protonmail.com>
2021-02-04 15:33:56 -0700

    MVP

--- pages/index.py
+++ pages/index.py
@@ -7,48 +7,58 @@ import pandas as pd
 from dash.dependencies import Input, Output, State
 # Imports from this application
 from app import app, pipeline
+import keras
+import numpy as np
 
+model = keras.models.load_model('data/kerasmodel')
+model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 
-def get_prediction(occupancy, listing_count,
-                   availability_90, num_reviews,
-                   num_bath, num_bed, num_bedroom,
-                   min_nights, room_type, property_type, bathroom_type,
-                   has_about, has_neighborhood_overview,
-                   account_age, has_profile_pic, identity_verified,
-                   is_superhost, instant_bookable,
-                   neighbourhood):
+# Model parameters
+# columns = ['host_total_listings_count', 
+# 'latitude', 
+# 'longitude', 
+# 'accommodates',
+# 'bedrooms', 
+# 'beds', 
+# 'minimum_nights', 
+# 'maximum_nights', 
+# 'availability_30', 
+# 'availability_365',
+# 'number_of_reviews', 
+# 'bathrooms']
+
+def get_prediction(host_listings, latitude,
+                  longitude, accommodates,
+                  bedrooms, beds, num_reviews,
+                  avail_30, avail_365, max_nights, min_nights,
+                  num_bath):
     """
     Function for checking if all features are correctly entered and to return
     a message to the user to fix their input if not. Otherwise returns a price
     prediction by passing features to the price prediction model.
     """
-    df = pd.DataFrame(data={'occupancy': occupancy,
-                            'listing_count': listing_count,
-                            'availability_90': availability_90,
+    df = pd.DataFrame(data={'host_listings': host_listings,
+                            'latitude': latitude,
+                            'longitude': longitude,
+                            'accommodates': accommodates,
+                            'bedrooms': bedrooms,
+                            'beds': beds,
                             'num_reviews': num_reviews,
-                            'num_bath': num_bath,
-                            'num_bed': num_bed,
-                            'num_bedroom': num_bedroom,
+                            'avail_30': avail_30,
+                            'avail_365': avail_365,
+                            'max_nights': max_nights,
                             'min_nights': min_nights,
-                            'room_type': room_type,
-                            'property_type': property_type,
-                            'bathroom_type': bathroom_type,
-                            'has_about': has_about,
-                            'has_neighborhood_overview': has_neighborhood_overview,
-                            'account_age': account_age,
-                            'has_profile_pic': has_profile_pic,
-                            'identity_verified': identity_verified,
-                            'is_superhost': is_superhost,
-                            'instant_bookable': instant_bookable,
-                            'neighbourhood': neighbourhood
+                            'num_bath': num_bath,
                             },
                       index=[0]
                       )
     if df.isnull().values.any():
         return dcc.Markdown('All fields must be filled!')
     else:
-        prediction = pipeline.predict(df)
-        return dcc.Markdown('You should charge ${} per night'.format(round(prediction[0]), 2))
+        X = np.asarray(df).astype(np.float64)
+        prediction = model.predict(X)
+        # prediction = pipeline.predict(df)
+        return dcc.Markdown('You should charge ${} per night.'.format(round(float(prediction[0])), 2))
 
 
 def get_options(options):
@@ -64,67 +74,73 @@ def get_options(options):
 # Column for first 5 features
 column1 = dbc.Col(
     [
-        dcc.Input(id='occupancy', type='number',
-                  placeholder='Max Occupancy', min=1, step=1),
+        dcc.Input(id='host_listings', type='number',
+                  placeholder='Host Listings', min=1, step=1),
         html.Br(), html.Br(),
-        dcc.Input(id='listing_count', type='number',
-                  placeholder='Number of Listings', min=1, step=1),
+        dcc.Input(id='latitude', type='number',
+                  placeholder='Latitude', min=1, step=1),
         html.Br(), html.Br(),
-        dcc.Input(id='availability_90', type='number',
-                  placeholder='Next 90 Days Available', min=1,
+        dcc.Input(id='longitude', type='number',
+                  placeholder='Longitude', min=1,
                   step=1),
         html.Br(), html.Br(),
-        dcc.Input(id='min_nights', type='number',
-                  placeholder='Minimum Nights', min=1, step=1),
+        dcc.Input(id='accommodates', type='number',
+                  placeholder='Accommodates', min=1, step=1),
         html.Br(), html.Br(),
-        dcc.Dropdown(
-            id='bathroom_type',
-            options=get_options(['private', 'regular', 'shared']),
-            searchable=False,
-            clearable=False,
-            placeholder='Bathroom Type'
-        ),
+        dcc.Input(id='bedrooms', type='number',
+                  placeholder='Bedrooms', min=1, step=1),
         html.Br(), html.Br(),
-        dcc.Dropdown(
-            id='neighbourhood',
-            options=get_options(['Yau Tsim Mong', 'Yuen Long', 'Wan Chai', 'Central & Western',
-                                 'Eastern', 'Kowloon City', 'Sha Tin', 'Sham Shui Po', 'Islands',
-                                 'Sai Kung', 'Wong Tai Sin', 'North', 'Tsuen Wan', 'Kwun Tong',
-                                 'Southern', 'Tuen Mun', 'Kwai Tsing', 'Tai Po']),
-            searchable=False,
-            clearable=False,
-            placeholder='Neighbourhood'
-        ),
+        dcc.Input(id='beds', type='number',
+                  placeholder='Beds', min=1, step=1),
         html.Br(), html.Br(),
-        dcc.RadioItems(
-            id='identity_verified',
-            options=[
-                {'label': 'Identity Verified', 'value': 1},
-                {'label': 'Identity Not Verified', 'value': 0},
-            ],
-            value=1,
-            labelStyle={'display': 'inline-block', 'padding': '10px'}
-        ),
-        html.Br(), html.Br(),
-        dcc.RadioItems(
-            id='is_superhost',
-            options=[
-                {'label': 'Superhost', 'value': 1},
-                {'label': 'Not Superhost', 'value': 0},
-            ],
-            value=1,
-            labelStyle={'display': 'inline-block', 'padding': '10px'}
-        ),
-        html.Br(), html.Br(),
-        dcc.RadioItems(
-            id='instant_bookable',
-            options=[
-                {'label': 'Instant Booking', 'value': 1},
-                {'label': 'No Instant Booking', 'value': 0},
-            ],
-            value=1,
-            labelStyle={'display': 'inline-block', 'padding': '10px'}
-        ),
+        # dcc.Dropdown(
+        #     id='bathroom_type',
+        #     options=get_options(['private', 'regular', 'shared']),
+        #     searchable=False,
+        #     clearable=False,
+        #     placeholder='Bathroom Type'
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.Dropdown(
+        #     id='neighbourhood',
+        #     options=get_options(['Yau Tsim Mong', 'Yuen Long', 'Wan Chai', 'Central & Western',
+        #                          'Eastern', 'Kowloon City', 'Sha Tin', 'Sham Shui Po', 'Islands',
+        #                          'Sai Kung', 'Wong Tai Sin', 'North', 'Tsuen Wan', 'Kwun Tong',
+        #                          'Southern', 'Tuen Mun', 'Kwai Tsing', 'Tai Po']),
+        #     searchable=False,
+        #     clearable=False,
+        #     placeholder='Neighbourhood'
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.RadioItems(
+        #     id='identity_verified',
+        #     options=[
+        #         {'label': 'Identity Verified', 'value': 1},
+        #         {'label': 'Identity Not Verified', 'value': 0},
+        #     ],
+        #     value=1,
+        #     labelStyle={'display': 'inline-block', 'padding': '10px'}
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.RadioItems(
+        #     id='is_superhost',
+        #     options=[
+        #         {'label': 'Superhost', 'value': 1},
+        #         {'label': 'Not Superhost', 'value': 0},
+        #     ],
+        #     value=1,
+        #     labelStyle={'display': 'inline-block', 'padding': '10px'}
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.RadioItems(
+        #     id='instant_bookable',
+        #     options=[
+        #         {'label': 'Instant Booking', 'value': 1},
+        #         {'label': 'No Instant Booking', 'value': 0},
+        #     ],
+        #     value=1,
+        #     labelStyle={'display': 'inline-block', 'padding': '10px'}
+        # ),
     ]
 )
 
@@ -134,73 +150,79 @@ column2 = dbc.Col(
         dcc.Input(id='num_reviews', type='number',
                   placeholder='Number of Reviews', step=1),
         html.Br(), html.Br(),
+        dcc.Input(id='avail_30', type='number',
+                  placeholder='Availability 30', min=1, step=1),
+        html.Br(), html.Br(),
+        dcc.Input(id='avail_365', type='number',
+                  placeholder='Availability 365', min=1, step=0.5),
+        html.Br(), html.Br(),
+        dcc.Input(id='min_nights', type='number',
+                  placeholder='Min nights', min=1, step=0.5),
+        html.Br(), html.Br(),
+        dcc.Input(id='max_nights', type='number',
+                  placeholder='Max nights', min=1, step=0.5),
+        html.Br(), html.Br(),
         dcc.Input(id='num_bath', type='number',
                   placeholder='Number of Bathrooms', min=1, step=0.5),
         html.Br(), html.Br(),
-        dcc.Input(id='num_bed', type='number',
-                  placeholder='Number of Beds', min=1, step=1),
-        html.Br(), html.Br(),
-        dcc.Input(id='num_bedroom', type='number',
-                  placeholder='Number of Bedrooms', min=1, step=0.5),
-        html.Br(), html.Br(),
-        dcc.Dropdown(
-            id='room_type',
-            options=get_options(
-                ['Entire home/apt', 'Private room', 'Shared room', 'Hotel room']),
-            searchable=False,
-            clearable=False,
-            placeholder='Room Type'
-        ),
-        html.Br(), html.Br(),
-        dcc.Dropdown(
-            id='property_type',
-            options=get_options(['apartment', 'guesthouse', 'bed and breakfast', 'bungalow',
-                                 'hotel', 'guest suite', 'condominium', 'boutique hotel', 'house',
-                                 'hostel', 'serviced apartment', 'loft', 'townhouse', 'Entire loft',
-                                 'Entire apartment', 'Entire condominium',
-                                 'Entire serviced apartment', 'treehouse', 'chalet', 'Entire house',
-                                 'Entire guest suite', 'Private room', 'aparthotel', 'cottage',
-                                 'castle', 'cabin', 'tiny house', 'resort', 'minsu', 'hut',
-                                 'kezhan', 'villa', 'earth house', 'nature lodge', 'Tiny house',
-                                 'Entire home/apt', 'igloo', 'Entire guesthouse', 'Casa particular',
-                                 'Entire townhouse', 'Entire place', 'Entire bungalow',
-                                 'Earth house', 'casa particular', 'Entire cottage', 'pension',
-                                 'Campsite', 'Island', 'Boat', 'Cave', 'Tent', 'Castle', 'Pension',
-                                 'Dome house', 'Entire villa', 'Farm stay', 'Shared room']),
-            searchable=False,
-            clearable=False,
-            placeholder='Property Type'
-        ),
-        html.Br(), html.Br(),
-        dcc.RadioItems(
-            id='has_about',
-            options=[
-                {'label': 'Has About', 'value': 1},
-                {'label': 'No About', 'value': 0},
-            ],
-            value=1,
-            labelStyle={'display': 'inline-block', 'padding': '10px'}
-        ),
-        html.Br(), html.Br(),
-        dcc.RadioItems(
-            id='has_neighborhood_overview',
-            options=[
-                {'label': 'Has Neighborhood Overview', 'value': 1},
-                {'label': 'No Neighborhood Overview', 'value': 0},
-            ],
-            value=1,
-            labelStyle={'display': 'inline-block', 'padding': '10px'}
-        ),
-        html.Br(), html.Br(),
-        dcc.RadioItems(
-            id='has_profile_pic',
-            options=[
-                {'label': 'Has Profile Pic', 'value': 1},
-                {'label': 'No Profile Pic', 'value': 0},
-            ],
-            value=1,
-            labelStyle={'display': 'inline-block', 'padding': '10px'}
-        ),
+        # dcc.Dropdown(
+        #     id='room_type',
+        #     options=get_options(
+        #         ['Entire home/apt', 'Private room', 'Shared room', 'Hotel room']),
+        #     searchable=False,
+        #     clearable=False,
+        #     placeholder='Room Type'
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.Dropdown(
+        #     id='property_type',
+        #     options=get_options(['apartment', 'guesthouse', 'bed and breakfast', 'bungalow',
+        #                          'hotel', 'guest suite', 'condominium', 'boutique hotel', 'house',
+        #                          'hostel', 'serviced apartment', 'loft', 'townhouse', 'Entire loft',
+        #                          'Entire apartment', 'Entire condominium',
+        #                          'Entire serviced apartment', 'treehouse', 'chalet', 'Entire house',
+        #                          'Entire guest suite', 'Private room', 'aparthotel', 'cottage',
+        #                          'castle', 'cabin', 'tiny house', 'resort', 'minsu', 'hut',
+        #                          'kezhan', 'villa', 'earth house', 'nature lodge', 'Tiny house',
+        #                          'Entire home/apt', 'igloo', 'Entire guesthouse', 'Casa particular',
+        #                          'Entire townhouse', 'Entire place', 'Entire bungalow',
+        #                          'Earth house', 'casa particular', 'Entire cottage', 'pension',
+        #                          'Campsite', 'Island', 'Boat', 'Cave', 'Tent', 'Castle', 'Pension',
+        #                          'Dome house', 'Entire villa', 'Farm stay', 'Shared room']),
+        #     searchable=False,
+        #     clearable=False,
+        #     placeholder='Property Type'
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.RadioItems(
+        #     id='has_about',
+        #     options=[
+        #         {'label': 'Has About', 'value': 1},
+        #         {'label': 'No About', 'value': 0},
+        #     ],
+        #     value=1,
+        #     labelStyle={'display': 'inline-block', 'padding': '10px'}
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.RadioItems(
+        #     id='has_neighborhood_overview',
+        #     options=[
+        #         {'label': 'Has Neighborhood Overview', 'value': 1},
+        #         {'label': 'No Neighborhood Overview', 'value': 0},
+        #     ],
+        #     value=1,
+        #     labelStyle={'display': 'inline-block', 'padding': '10px'}
+        # ),
+        # html.Br(), html.Br(),
+        # dcc.RadioItems(
+        #     id='has_profile_pic',
+        #     options=[
+        #         {'label': 'Has Profile Pic', 'value': 1},
+        #         {'label': 'No Profile Pic', 'value': 0},
+        #     ],
+        #     value=1,
+        #     labelStyle={'display': 'inline-block', 'padding': '10px'}
+        # ),
     ]
 )
 
@@ -259,7 +281,7 @@ layout = dbc.Container(
     fluid=True,
     children=[
         dbc.Row([column1, column2]),
-        dbc.Row([column_slider]),
+        # dbc.Row([column_slider]),
         dbc.Row([prediction_column]),
         dbc.Row([column_button])
     ],
@@ -271,41 +293,28 @@ layout = dbc.Container(
 @app.callback(
     Output('output-submit', 'children'),
     Input('btn-submit', 'n_clicks'),
-    State('occupancy', 'value'),
-    State('listing_count', 'value'),
-    State('availability_90', 'value'),
+    State('host_listings', 'value'),
+    State('latitude', 'value'),
+    State('longitude', 'value'),
+    State('accommodates', 'value'),
+    State('bedrooms', 'value'),
+    State('beds', 'value'),
     State('num_reviews', 'value'),
-    State('num_bath', 'value'),
-    State('num_bed', 'value'),
-    State('num_bedroom', 'value'),
+    State('avail_30', 'value'),
+    State('avail_365', 'value'),
+    State('max_nights', 'value'),
     State('min_nights', 'value'),
-    State('room_type', 'value'),
-    State('property_type', 'value'),
-    State('bathroom_type', 'value'),
-    State('has_about', 'value'),
-    State('has_neighborhood_overview', 'value'),
-    State('account_age', 'value'),
-    State('has_profile_pic', 'value'),
-    State('identity_verified', 'value'),
-    State('is_superhost', 'value'),
-    State('instant_bookable', 'value'),
-    State('neighbourhood', 'value'),
+    State('num_bath', 'value'),
 )
-def update_output(clicks, occupancy, listing_count,
-                  availability_90, num_reviews,
-                  num_bath, num_bed, num_bedroom,
-                  min_nights, room_type, property_type, bathroom_type,
-                  has_about, has_neighborhood_overview,
-                  account_age, has_profile_pic, identity_verified,
-                  is_superhost, instant_bookable,
-                  neighbourhood):
+def update_output(clicks, host_listings, latitude,
+                  longitude, accommodates,
+                  bedrooms, beds, num_reviews,
+                  avail_30, avail_365, max_nights, min_nights,
+                  num_bath):
     if clicks:
         # Return prediction when button is clicked
-        return get_prediction(occupancy, listing_count,
-                              availability_90, num_reviews,
-                              num_bath, num_bed, num_bedroom,
-                              min_nights, room_type, property_type, bathroom_type,
-                              has_about, has_neighborhood_overview,
-                              account_age, has_profile_pic, identity_verified,
-                              is_superhost, instant_bookable,
-                              neighbourhood)
+        return get_prediction(host_listings, latitude,
+                  longitude, accommodates,
+                  bedrooms, beds, num_reviews,
+                  avail_30, avail_365, max_nights, min_nights,
+                  num_bath)
