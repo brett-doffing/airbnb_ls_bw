a568b2b0142c1580cb23ce888840b7c122f0da3e
Brett <doffing81@protonmail.com>
2021-02-04 14:31:33 -0700

    Working model

--- data/model.py
+++ data/model.py
@@ -15,6 +15,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 
+from keras.models import model_from_json
+
 df = pd.read_csv('https://raw.githubusercontent.com/leibo411/airbnb_ls_bw/main/data/listings3.csv')
 
 def wrangle_bathrooms_text(x):
@@ -37,7 +39,7 @@ df['bathrooms'] = df.bathrooms_text.apply(wrangle_bathrooms_text)
 def load_data():
   # df = pd.read_csv('https://raw.githubusercontent.com/leibo411/airbnb_ls_bw/main/data/listings3.csv')
   df_number = df.select_dtypes(include='number')
-  df_number = df_number.drop(columns=['id', 'scrape_id', 'neighbourhood_group_cleansed', 'bathrooms', 'calendar_updated', 'license', 'availability_60', 'number_of_reviews_l30d', 
+  df_number = df_number.drop(columns=['id', 'scrape_id', 'neighbourhood_group_cleansed', 'calendar_updated', 'license', 'availability_60', 'number_of_reviews_l30d', 
   'maximum_nights_avg_ntm', 'review_scores_communication', 'review_scores_location', 'availability_90', 'review_scores_checkin', 'calculated_host_listings_count_entire_homes', 
   'number_of_reviews_ltm', 'minimum_minimum_nights', 'maximum_minimum_nights','minimum_maximum_nights', 'maximum_maximum_nights', 'minimum_nights_avg_ntm', 'review_scores_cleanliness', 
   'review_scores_rating', 'review_scores_accuracy', 'review_scores_value', 'calculated_host_listings_count', 'calculated_host_listings_count_private_rooms', 'calculated_host_listings_count_shared_rooms', 
@@ -51,7 +53,6 @@ def load_data():
 
 df = load_data()
 
-# print(df.head())
 
 # identify the target for the model
 X = np.array(df.drop(columns=['price', 'host_location'])) # might have to be a numpy array, original code was  X = np.array(df.drop(columns='Survived'))
@@ -70,9 +71,11 @@ columns = ['host_total_listings_count', 'latitude', 'longitude', 'accommodates',
 
 # Building the model
 nn2 = models.Sequential()
-nn2.add(layers.Dense(128, input_shape=(X_train.shape[1],), activation='relu'))
+nn2.add(layers.Dense(512, input_shape=(X_train.shape[1],), activation='softmax'))
+nn2.add(layers.Dropout(0.2))
 nn2.add(layers.Dense(256, activation='relu'))
-nn2.add(layers.Dense(256, activation='relu'))
+nn2.add(layers.Dropout(0.2))
+# nn2.add(layers.Dense(256, activation='relu'))
 nn2.add(layers.Dense(128, activation='relu'))
 nn2.add(layers.Dense(16, activation='relu'))
 nn2.add(layers.Dense(1, activation='linear'))
@@ -89,16 +92,27 @@ print(nn2.summary())
 # Training the model
 nn2_history = nn2.fit(X_train,
                   y_train,
-                  epochs=1000,
+                  epochs=500,
                   batch_size=256,
                   validation_split = 0.1)
+
+# Save model
+nn2.save('kerasmodel')
+
+# Load model
+# model = keras.models.load_model('kerasmodel')
+# model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+# # print(model.summary())
 
 # dummy_data = {'host_total_listing_count': [1], 'latitude': [17.234], 'longitude': [17.234], 'accommodates': [4], 'bedrooms': [3], 'beds': [3], 
 # 'minimum_nights': [2], 'maximum_nights': [90], 'availability_30': [20], 'availability_365': [300], 'number_of_reviews': [40], 'bathrooms': [1]}
 # df_dummy = pd.DataFrame(data=dummy_data)
 
+# df_dummy['latitude'] = df_dummy['latitude'].astype(float)
+# df_dummy['longitude'] = df_dummy['longitude'].astype(float)
+
 # df_dummy = pd.DataFrame(data=dummy_data, dtype=np.int64)
 
 # X = np.asarray(df_dummy).astype(np.float64)
 
-# nn2.predict(X)
+# print(model.predict(X))
